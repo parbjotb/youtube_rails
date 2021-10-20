5686b8d9b618340512cdd84e5b444a72b32b3a80
parbjotb <parbjotb@gmail.com>
2021-10-20 15:09:08 -0500

    Added csv parsing with youtube video info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,24 @@
+require "csv"
+
+#database should be wiped and recreated first
+Video.delete_all
+ChannelCreator.delete_all
+
+# now we take in the csv file and turn it into something we can parse
+filename = Rails.root.join("db/top_videos.csv")
+
+puts "Loading videos from the CSV file: #{filename}"
+
+# we can use csv_data variable to parse
+csv_data = File.read(filename)
+# this line below is what reads in the csv file
+# need the liberal_parsing bit cause of whitespace in the titles, it was throwing errors
+#https://github.com/ruby/csv/issues/91
+videos = CSV.parse(csv_data, headers:true, liberal_parsing: true, encoding: "utf-8")
+
+videos.each do |v|
+  puts v["title"]
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +26,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
