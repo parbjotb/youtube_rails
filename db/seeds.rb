2277daf4c8eaf0808efe57d2e0c9f3257e2eb3c5
parbjotb <parbjotb@gmail.com>
2021-10-21 13:04:30 -0500

    Modified video model to refactor validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require "csv"
 
-#database should be wiped and recreated first
+# database should be wiped and recreated first
 Video.delete_all
 ChannelCreator.delete_all
 
@@ -9,26 +9,65 @@ filename = Rails.root.join("db/top_videos.csv")
 
 puts "Loading videos from the CSV file: #{filename}"
 
-# we can use csv_data variable to parse
+# # we can use csv_data variable to parse
 csv_data = File.read(filename)
-# this line below is what reads in the csv file
-# need the liberal_parsing bit cause of whitespace in the titles, it was throwing errors
-#https://github.com/ruby/csv/issues/91
-videos = CSV.parse(csv_data, headers:true, liberal_parsing: true, encoding: "utf-8")
+# # this line below is what reads in the csv file
+# # need the liberal_parsing bit cause of whitespace in the titles, it was throwing errors
+# #https://github.com/ruby/csv/issues/91
+videos = CSV.parse(csv_data, headers: true, liberal_parsing: true, encoding: "utf-8")
 
-videos.each do |v|
-  #puts v["title"]
-  # now we will create the creators as we loop through
-  # find or create by makes it so it will only create the channel once instead of duplicate entries
+# only takes in first 400 rows of my csv file which has like 65k rows
+(0..400).each do |i|
+  v = videos[i]
   channel_creator = ChannelCreator.find_or_create_by(name: v["channel_title"])
 
   # if the channel exists and is valid then do something
-  if channel_creator && channel_creator.valid?
+  if channel_creator&.valid?
     # create video here
+    # this should allow us to create videos off the channel
+    # thus wiring up the foreign key without us needing to specify it
+    video = channel_creator.videos.create(
+      title:       v["title"],
+      views:       v["views"],
+      description: v["description"]
+    )
+
+    # this syntax below is equal to
+    # movie && movie.valid?
+    # if it exists and is valid
+    puts "Invalid video #{v['title']}" unless video&.valid?
   else
-    puts "Invalid channel creator,#{v["channel_title"]} for video: #{v["title"]}"
+    puts "Invalid channel creator,#{v['channel_title']} for video: #{v['title']}"
   end
 end
+
+puts "Created #{ChannelCreator.count} youtube channels"
+puts "Created #{Video.count} videos"
+# videos.each do |v|
+#   # puts v["title"]
+#   # now we will create the creators as we loop through
+#   # find or create by makes it so it will only create the channel once instead of duplicate entries
+#   channel_creator = ChannelCreator.find_or_create_by(name: v["channel_title"])
+
+#   # if the channel exists and is valid then do something
+#   if channel_creator && channel_creator.valid?
+#     # create video here
+#     # this should allow us to create videos off the channel
+#     # thus wiring up the foreign key without us needing to specify it
+#     video = channel_creator.videos.create(
+#       title:       v["title"],
+#       views:       v["views"],
+#       description: v["description"]
+#     )
+
+#     # this syntax below is equal to
+#     # movie && movie.valid?
+#     # like line 26 above in the if statement, if it exists and is valid
+#     puts "Invalid video #{v['title']}" unless video&.valid?
+#   else
+#     puts "Invalid channel creator,#{v['channel_title']} for video: #{v['title']}"
+#   end
+# end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -36,4 +75,3 @@ end
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
