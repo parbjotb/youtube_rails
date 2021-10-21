9a7b0e64c1cdd8ead8e50f0a2de6bc855a7bb92b
parbjotb <parbjotb@gmail.com>
2021-10-21 13:37:55 -0500

    Added index and show view files and populated them with data

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,10 @@
 class VideosController < ApplicationController
   def index
+    # now videos will be passed to the index view
+    @videos = Video.includes(:channel_creator).all
   end
 
   def show
+    @video = Video.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'videos/index'
-  get 'videos/show'
-  get 'channel_creators/index'
-  get 'channel_creators/show'
+  # resources links to controller, only show the index and show pages
+  resources :videos, only: %i[index show]
+  resources :channel_creators, only: %i[index show]
+  # get 'videos/index'
+  # get 'videos/show'
+  # get 'channel_creators/index'
+  # get 'channel_creators/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
