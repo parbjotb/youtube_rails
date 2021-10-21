3fb48d2d55cad8147d8b22c92c0c9a30fc55af92
parbjotb <parbjotb@gmail.com>
2021-10-21 17:22:35 -0500

    modified channels controller and added views

--- app/controllers/channel_creators_controller.rb
+++ app/controllers/channel_creators_controller.rb
@@ -1,7 +1,13 @@
 class ChannelCreatorsController < ApplicationController
   def index
+    @channel_creators = ChannelCreator.select("channel_creators.*")
+                                      .select("COUNT(channel_creators.id) as video_count")
+                                      .left_joins(:videos)
+                                      .group("channel_creators.id")
+                                      .order("video_count DESC")
   end
 
   def show
+    @channel_creator = ChannelCreator.find(params[:id])
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
     @videos = Video.includes(:channel_creator).order("views DESC").limit(10)
-    # @channel_creators =
+    @channel_creators = ChannelCreator.select("channel_creators.*")
+                                      .select("COUNT(channel_creators.id) as video_count")
+                                      .left_joins(:videos)
+                                      .group("channel_creators.id")
+                                      .order("video_count DESC")
+                                      .limit(10)
   end
 end
