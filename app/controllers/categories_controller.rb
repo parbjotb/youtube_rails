b58d48641b40ac7ff43d504632ef8713309a92bb
parbjotb <parbjotb@gmail.com>
2021-11-02 15:01:17 -0500

    added categories controllers functions for index and show and views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,11 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.paginate(page: params[:page], per_page: 10)
   end
 
   def show
+    # puts params
+    # logger.debug params[:id]
+    @videos = Video.where(category_id: params[:id])
   end
 end
